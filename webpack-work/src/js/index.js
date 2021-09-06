f530745a002b3eacd84d0af87530765bd7e48e43
Litinsci <litovchenko.73@list.ru>
2021-09-06 17:19:06 +0300

    tabs

--- webpack-work/src/js/index.js
+++ webpack-work/src/js/index.js
@@ -4,6 +4,7 @@ import { Advantages } from './components/itHubPage/advantages';
 import { CareerUpgrade } from './components/itHubPage/upgrade';
 import { VideoPlayer } from './components/itHubPage/videoPlayer';
 import './components/animation';
+import {Tab} from "./components/itHubPage/Tab";
 // Перенос изображений
 require.context('../images', true, /\.(png|jpg|svg|gif)$/);
 require.context('../fonts', true, /\.(ttf|woff|woff2)$/);
@@ -14,12 +15,14 @@ const listingPage = document.querySelector('.listing');
 const ourAdvantages = document.querySelector('.why-are-we');
 const upgradeSection = document.querySelector('.upgrade');
 const videoPlayerBlock = document.querySelector('.find');
+const tabContainer = document.querySelector('.tab-content');
 
 if (itHubPage && itHubPage !== null && itHubPage !== undefined) {
   new Vacancy(profession);
   new Advantages(ourAdvantages);
   new CareerUpgrade(upgradeSection);
   new VideoPlayer(videoPlayerBlock);
+  new Tab(tabContainer, 'direction__direction-item-active');
 }
 
 if (listingPage && listingPage !== null && listingPage !== undefined) {
