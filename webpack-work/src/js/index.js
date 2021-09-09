fb308ef307c106f7568fe2a20f40f8c96a763271
Vladimir <shadrov_vladimir@mail.ru>
2021-09-09 11:35:26 +0300

    Добавлена страница анкеты

--- webpack-work/src/js/index.js
+++ webpack-work/src/js/index.js
@@ -3,9 +3,10 @@ import { Listing } from './components/vacancyListingPage/listing';
 import { Advantages } from './components/itHubPage/advantages';
 import { CareerUpgrade } from './components/itHubPage/upgrade';
 import { VideoPlayer } from './components/itHubPage/videoPlayer';
-import {Tab} from "./components/itHubPage/Tab";
+import { Tab } from './components/itHubPage/Tab';
 import './components/animation';
-import {Select} from "./components/itHubPage/select";
+import { Select } from './components/itHubPage/select';
+import { Form } from './components/itHubPage/form';
 
 // Перенос изображений
 require.context('../images', true, /\.(png|jpg|svg|gif)$/);
@@ -18,12 +19,14 @@ const ourAdvantages = document.querySelector('.why-are-we');
 const upgradeSection = document.querySelector('.upgrade');
 const videoPlayerBlock = document.querySelector('.find');
 const tabContainer = document.querySelector('.tab-content');
+const form = document.querySelector('.form');
 
 if (itHubPage && itHubPage !== null && itHubPage !== undefined) {
   new Vacancy(profession);
   new Advantages(ourAdvantages);
   new CareerUpgrade(upgradeSection);
   new VideoPlayer(videoPlayerBlock);
+  new Form(form);
 }
 
 new Tab(tabContainer, 'direction__direction-item-active');
@@ -34,7 +37,31 @@ const selectSecond = document.getElementById('custom-select-second');
 new Select(selectGreen);
 new Select(selectSecond);
 
-
 if (listingPage && listingPage !== null && listingPage !== undefined) {
   new Listing(listingPage);
+}
+
+document.body.addEventListener('click', (event) => {
+  if (event.target.tagName.toLowerCase() === 'a') {
+    event.preventDefault();
+  }
+
+  if (
+    event.target.classList.contains('it-header__button') ||
+    event.target.classList.contains('intern__header-link')
+  ) {
+    openApplicationForm();
+  }
+});
+
+// Открыть анкету выбора вакансии
+function openApplicationForm() {
+  const form = document.querySelector('.form');
+
+  form.classList.add('form-active');
+
+  setTimeout(() => {
+    document.body.style.overflow = 'hidden';
+    form.firstElementChild.style.background = 'rgba(0, 0, 0, 0.8)';
+  }, 300);
 }
