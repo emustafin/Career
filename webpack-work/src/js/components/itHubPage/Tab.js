9b3e41fdaefa39329412084d99544ed922e7f751
Litinsci <litovchenko.73@list.ru>
2021-09-13 10:49:39 +0300

    rework typed

--- webpack-work/src/js/components/itHubPage/Tab.js
+++ webpack-work/src/js/components/itHubPage/Tab.js
@@ -1,62 +1,4 @@
 /*
- # tab-content - внешняя обёртка для всех табов
-
- # tab-list - список табов необходимо указать
- данный класс в разметке на контейнер который
- содержит переключатели
-
- # tab-item-content - контейнер необходимый для
- содержания табов
- direction__direction-item-active
-
- # tab-choice - отображение выбранного таба
-
- # data-tab-uid - data атрибут по которому осуществляется переключение
-*/
-// export class Tab {
-//   constructor(el, activeClass) {
-//     this.el = el;
-//     if ((this.el == null || undefined)) return;
-//     this.activeClass = activeClass
-//     this.tabList = el.querySelector('.tab-list');
-//     this.tabChoice  = el.querySelector('.tab-choice');
-//     this.tabItemContent = el.querySelectorAll('.tab-item-content');
-//
-//     this.init();
-//     this.tabList.querySelectorAll('.tab-list-item').forEach(el => {
-//       el.onclick = this.handleClick.bind(this);
-//     });
-//   }
-//
-//   init() {
-//     this.tabList.querySelector('.tab-list-item').classList.add(this.activeClass);
-//     this.tabItemContent.forEach((el, i) => {
-//       if (i == 0) el.style.display = 'block';
-//       else el.style.display = 'none';
-//     });
-//   }
-//
-//   handleClick(e) {
-//     this.tabList.querySelectorAll('.tab-list-item').forEach(el => {
-//       el.classList.remove(this.activeClass);
-//     })
-//     e.currentTarget.classList.add(this.activeClass);
-//
-//     this.tabItemContent.forEach(el => {
-//       if (el.dataset.tabUid == e.currentTarget.dataset.tabUid) el.style.display = 'block';
-//       else el.style.display = 'none';
-//     });
-//   }
-// }
-
-
-
-
-//
-// const tabContainer = document.querySelector('.tab-content');
-// new Tab(tabContainer, 'direction__direction-item-active', document.getElementById('tab-list__select'));
-
-/*
   el - entri point for weap/container tabs content
   controller - DOM knot whith the carries out control ower el
   activeClass - toggl class for active elTab
@@ -76,7 +18,6 @@ export class Tab {
   }
 
   init () {
-    console.log(this.controller);
     this.contentList.forEach((el, i) => {
       if (i == 0) el.style.display = 'block';
       else el.style.display = 'none';
