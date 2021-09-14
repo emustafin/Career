100e1b1c5db605d39c9e518d0110b9b73806070b
Mel-SON <omelyanchenko.semen@yandex.ru>
2021-09-14 18:46:16 +0300

    Добавляет скрипт при закрытии попапа, который восстанавливает оригинальную ссылку

--- assets/js/popup_vacancy.js
+++ assets/js/popup_vacancy.js
@@ -1,12 +1,10 @@
-
-
 $('.profession__job-title').on( 'click', function(e){
     
     var vacancy_info = $(this).attr('data-info');
     vacancy_info = jQuery.parseJSON( vacancy_info );
-
+    
     var origin_location = window.location;
-
+    
     $('.flyout .vacancy__headline-title').attr( 'origin_url', origin_location );
     $('.flyout .vacancy__headline-title').html( vacancy_info.title );
     $('.flyout .vacancy__video-container img').attr( "src", vacancy_info.img );
@@ -21,6 +19,16 @@ $('.profession__job-title').on( 'click', function(e){
     $('.flyout #vaccat_info_vacancy').html( vacancy_info.vaccat );
     $('.flyout #town_info_vacancy').html( vacancy_info.town );
     $('.flyout #experience_important').html( vacancy_info.experience );
-
+    
     window.history.pushState( '', '', vacancy_info.url );
+})
+
+$('.vacancy__header-head-link-main').on( 'click', function(e){
+    var origin_location = $('.flyout .vacancy__headline-title').attr( 'origin_url' );
+    window.history.pushState( '', '', origin_location );
+})
+
+$('.flyout__side-bar').on( 'click', function(e){
+    var origin_location = $('.flyout .vacancy__headline-title').attr( 'origin_url' );
+    window.history.pushState( '', '', origin_location );
 })
