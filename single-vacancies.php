47d9922070e8407b55a29f5a37e8d83dae997f0b
Mel-SON <omelyanchenko.semen@yandex.ru>
2021-09-15 14:26:59 +0300

    Натяжка новостей на странице вакансии

--- single-vacancies.php
+++ single-vacancies.php
@@ -207,11 +207,11 @@ if( get_field( 'map_full_adress', $post_id ) ){
                 alt="Map"
             />
 
-            <img
+            <!-- <img
                 class="vacancy__office-selector-image"
                 src="<?php echo THEME_URL; ?>/assets/images/flyout/flyout-office/map-selector.svg"
                 alt="Selector"
-            />
+            /> -->
             </div>
         </div>
 
@@ -451,101 +451,74 @@ if( get_field( 'map_full_adress', $post_id ) ){
 <!-- Vacancy News-block -->
 <div class="vacancy__news-block-desktop">
     <div class="vacancy__news">
-    <div class="vacancy__news-title">
-        <div class="vacancy__news-tabs">
-        <a href="#" class="vacancy__news-link vacancy__news-link-active"
-            >Новости</a
-        >
-        <a href="#" class="vacancy__news-link">События</a>
-        </div>
-        <div class="vacancy__news-arrows">
-        <button class="vacancy__news-btn-prev"></button>
-        <button class="vacancy__news-btn-next"></button>
-        </div>
-        <a href="#" class="vacancy__news-link-mobile"> Все новости </a>
-    </div>
-
-    <div class="vacancy__news-row">
-        <div class="vacancy__news-item">
-        <div class="vacancy__news-item-image-block">
-            <img
-            class="vacancy__news-image"
-            src="<?php echo THEME_URL; ?>/assets/images/flyout/flyout-news/news1.png"
-            alt="News"
-            />
+        <div class="vacancy__news-title">
+            <div class="vacancy__news-tabs">
+            <a href="#" class="vacancy__news-link vacancy__news-link-active"
+                >Новости</a
+            >
+            <a href="#" class="vacancy__news-link">События</a>
+            </div>
+            <div class="vacancy__news-arrows">
+            <button class="vacancy__news-btn-prev"></button>
+            <button class="vacancy__news-btn-next"></button>
+            </div>
+            <a href="#" class="vacancy__news-link-mobile"> Все новости </a>
         </div>
 
-        <a href="#" class="vacancy__news-title-info">
-            Группа «М.Видео-Эльдорадо» запустила программу стажировок
-            для программистов
-        </a>
-        <p class="vacancy__news-date-container">
-            <span class="vacancy__news-date">16</span>
-            <span class="vacancy__news-month">июля</span>
-            <span class="vacancy__news-year">2021</span>
-        </p>
-        </div>
+        <div class="vacancy__news-row">
+            <?php
+            $args = array(
+                'post_type'         => 'post',
+                'posts_per_page'    => 6,
+                'post_status'       => 'publish',
+                'order'             => 'DESC',
+                'order_by'          => 'date'
+            );
 
-        <div class="vacancy__news-item">
-        <div class="vacancy__news-item-image-block">
-            <img
-            class="vacancy__news-image"
-            src="<?php echo THEME_URL; ?>/assets/images/flyout/flyout-news/news2.png"
-            alt="News"
-            />
-        </div>
+            $actually_news = new WP_Query( $args );
 
-        <a href="#" class="vacancy__news-title-info">
-            Группа «М.Видео-Эльдорадо» запустила программу стажировок
-            для программистов
-        </a>
-        <p class="vacancy__news-date-container">
-            <span class="vacancy__news-date">16</span>
-            <span class="vacancy__news-month">июля</span>
-            <span class="vacancy__news-year">2021</span>
-        </p>
-        </div>
+            if ( $actually_news->have_posts() ) {
+                while ( $actually_news->have_posts() ) {
+                $actually_news->the_post();
+                $news_item_id = get_the_ID();
 
-        <div class="vacancy__news-item">
-        <div class="vacancy__news-item-image-block">
-            <img
-            class="vacancy__news-image"
-            src="<?php echo THEME_URL; ?>/assets/images/flyout/flyout-news/news1.png"
-            alt="News"
-            />
-        </div>
+                $thumbnail_url = get_the_post_thumbnail_url( $news_item_id, 'full' );
+                if( false == $thumbnail_url ){
+                    $thumbnail_url = THEME_URL .'/assets/images/default/post-image-default.jpg';
+                }
 
-        <a href="#" class="vacancy__news-title-info">
-            Группа «М.Видео-Эльдорадо» запустила программу стажировок
-            для программистов
-        </a>
-        <p class="vacancy__news-date-container">
-            <span class="vacancy__news-date">16</span>
-            <span class="vacancy__news-month">июля</span>
-            <span class="vacancy__news-year">2021</span>
-        </p>
-        </div>
+                if( null != get_field( 'url_from_habr' ) ){
+                    $url_from_habr = get_field( 'url_from_habr' );
+                } else{
+                    $url_from_habr = get_permalink();
+                }
+                ?>
 
-        <div class="vacancy__news-item">
-        <div class="vacancy__news-item-image-block">
-            <img
-            class="vacancy__news-image"
-            src="<?php echo THEME_URL; ?>/assets/images/flyout/flyout-news/news2.png"
-            alt="News"
-            />
-        </div>
 
-        <a href="#" class="vacancy__news-title-info">
-            Группа «М.Видео-Эльдорадо» запустила программу стажировок
-            для программистов
-        </a>
-        <p class="vacancy__news-date-container">
-            <span class="vacancy__news-date">16</span>
-            <span class="vacancy__news-month">июля</span>
-            <span class="vacancy__news-year">2021</span>
-        </p>
+                <div class="vacancy__news-item">
+                    <div class="vacancy__news-item-image-block">
+                        <img
+                        class="vacancy__news-image"
+                        src="<?php echo $thumbnail_url; ?>"
+                        alt="News"
+                        />
+                    </div>
+
+                    <a href="<?php echo $url_from_habr; ?>" class="vacancy__news-title-info">
+                        <?php the_title(); ?>
+                    </a>
+                    <p class="vacancy__news-date-container">
+                        <span class="vacancy__news-date"><?php echo get_the_date( 'j F Y', $news_item_id ); ?></span>
+                    </p>
+                </div>
+
+
+                <?php
+                }
+            }
+            ?>
+
         </div>
-    </div>
     </div>
 </div>
 <!-- //Vacancy News-block -->
@@ -553,101 +526,65 @@ if( get_field( 'map_full_adress', $post_id ) ){
 <!-- Vacancy New-block-mobile -->
 <div class="vacancy__news-block-mobile">
     <div class="vacancy__news">
-    <div class="vacancy__news-title">
-        <div class="vacancy__news-tabs">
-        <a href="#" class="vacancy__news-link vacancy__news-link-active"
-            >Новости</a
-        >
-        <a href="#" class="vacancy__news-link">События</a>
-        </div>
-        <div class="vacancy__news-arrows">
-        <button class="vacancy__news-btn-prev"></button>
-        <button class="vacancy__news-btn-next"></button>
-        </div>
-        <a href="#" class="vacancy__news-link-mobile"> Все новости </a>
-    </div>
-
-    <div class="vacancy__news-row">
-        <div class="vacancy__news-item">
-        <div class="vacancy__news-item-image-block">
-            <img
-            class="vacancy__news-image"
-            src="<?php echo THEME_URL; ?>/assets/images/flyout/flyout-news/news1.png"
-            alt="News"
-            />
+        <div class="vacancy__news-title">
+            <div class="vacancy__news-tabs">
+            <a href="#" class="vacancy__news-link vacancy__news-link-active"
+                >Новости</a
+            >
+            <a href="#" class="vacancy__news-link">События</a>
+            </div>
+            <div class="vacancy__news-arrows">
+            <button class="vacancy__news-btn-prev"></button>
+            <button class="vacancy__news-btn-next"></button>
+            </div>
+            <a href="#" class="vacancy__news-link-mobile"> Все новости </a>
         </div>
 
-        <a href="#" class="vacancy__news-title-info">
-            Группа «М.Видео-Эльдорадо» запустила программу стажировок
-            для программистов
-        </a>
-        <p class="vacancy__news-date-container">
-            <span class="vacancy__news-date">16</span>
-            <span class="vacancy__news-month">июля</span>
-            <span class="vacancy__news-year">2021</span>
-        </p>
-        </div>
+        <div class="vacancy__news-row">
+            
+            <?php
 
-        <div class="vacancy__news-item">
-        <div class="vacancy__news-item-image-block">
-            <img
-            class="vacancy__news-image"
-            src="<?php echo THEME_URL; ?>/assets/images/flyout/flyout-news/news2.png"
-            alt="News"
-            />
-        </div>
+            if ( $actually_news->have_posts() ) {
+                while ( $actually_news->have_posts() ) {
 
-        <a href="#" class="vacancy__news-title-info">
-            Группа «М.Видео-Эльдорадо» запустила программу стажировок
-            для программистов
-        </a>
-        <p class="vacancy__news-date-container">
-            <span class="vacancy__news-date">16</span>
-            <span class="vacancy__news-month">июля</span>
-            <span class="vacancy__news-year">2021</span>
-        </p>
-        </div>
+                $actually_news->the_post();
+                $news_item_id = get_the_ID();
 
-        <div class="vacancy__news-item">
-        <div class="vacancy__news-item-image-block">
-            <img
-            class="vacancy__news-image"
-            src="<?php echo THEME_URL; ?>/assets/images/flyout/flyout-news/news1.png"
-            alt="News"
-            />
-        </div>
+                $thumbnail_url = get_the_post_thumbnail_url( $news_item_id, 'full' );
+                if( false == $thumbnail_url ){
+                    $thumbnail_url = THEME_URL .'/assets/images/default/post-image-default.jpg';
+                }
 
-        <a href="#" class="vacancy__news-title-info">
-            Группа «М.Видео-Эльдорадо» запустила программу стажировок
-            для программистов
-        </a>
-        <p class="vacancy__news-date-container">
-            <span class="vacancy__news-date">16</span>
-            <span class="vacancy__news-month">июля</span>
-            <span class="vacancy__news-year">2021</span>
-        </p>
-        </div>
+                if( null != get_field( 'url_from_habr' ) ){
+                    $url_from_habr = get_field( 'url_from_habr' );
+                } else{
+                    $url_from_habr = get_permalink();
+                }
+                ?>
 
-        <div class="vacancy__news-item">
-        <div class="vacancy__news-item-image-block">
-            <img
-            class="vacancy__news-image"
-            src="<?php echo THEME_URL; ?>/assets/images/flyout/flyout-news/news2.png"
-            alt="News"
-            />
-        </div>
+                <div class="vacancy__news-item">
+                    <div class="vacancy__news-item-image-block">
+                        <img
+                        class="vacancy__news-image"
+                        src="<?php echo $thumbnail_url; ?>"
+                        alt="News"
+                        />
+                    </div>
 
-        <a href="#" class="vacancy__news-title-info">
-            Группа «М.Видео-Эльдорадо» запустила программу стажировок
-            для программистов
-        </a>
-        <p class="vacancy__news-date-container">
-            <span class="vacancy__news-date">16</span>
-            <span class="vacancy__news-month">июля</span>
-            <span class="vacancy__news-year">2021</span>
-        </p>
+                    <a href="<?php echo $url_from_habr; ?>" class="vacancy__news-title-info">
+                        <?php the_title(); ?>
+                    </a>
+                    <p class="vacancy__news-date-container">
+                        <span class="vacancy__news-date"><?php echo get_the_date( 'j F Y', $news_item_id ); ?></span>
+                    </p>
+                </div>
+
+                <?php
+                }
+            }
+            ?>
+
         </div>
-    </div>
     </div>
 </div>
 <!-- //Vacancy New-block-mobile -->
