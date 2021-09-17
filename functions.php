3999d4321da927c8a1703d4accb0ff067d47ed4c
Mel-SON <omelyanchenko.semen@yandex.ru>
2021-09-17 18:40:35 +0300

    #50 Добавляет функцию с хуком на cf7 для отправки данных в HR-систему

--- functions.php
+++ functions.php
@@ -22,33 +22,39 @@ TC();
 
 
 
-// add_action('wpcf7_mail_sent', function ($cf7) {
+add_action('wpcf7_mail_sent', function ($cf7) {
 
-// 	$submission = WPCF7_Submission::get_instance();
-// 	$posted_data = $submission->get_posted_data();
+	$submission = WPCF7_Submission::get_instance();
+	$posted_data = $submission->get_posted_data();
 	
-// 	$url = 'https://mvideo-api.huntflow.ru/account/2/applicants';
-// 	$params = array(
-// 		'last_name' => $posted_data['text-name'],
-// 		'phone' => $posted_data['mask-348'],
-// 		'email' => $posted_data['email-717'],
-// 		"externals" => [
-// 			[
-// 				"data"=> [
-// 					"body" => "Тестовый кандидат\nТестируем новый карьерный сайт"
-// 				],
-// 				"auth_type" => "NATIVE"
-// 			]
-// 		]
-// 	);
-// 	$result = file_get_contents($url, false, stream_context_create(array(
-// 		'http' => array(
-// 			'method'  => 'POST',
-// 			'header'  => 'Authorization: Bearer 66233397cc55c4714d52a78821c92910634e369cc3abee006304ccf6fb887097',
-// 			'header'  => 'Content-Type: application/json',
-// 			'content' => http_build_query($params)
-// 		)
-// 	)));
+	$url = 'https://mvideo-api.huntflow.ru/account/2/applicants';
+	$params = array(
+		'last_name' => $posted_data['text-name'],
+		'phone' => $posted_data['mask-348'],
+		'email' => $posted_data['email-717'],
+		"externals" => [
+			[
+				"data"=> [
+					"body" => "Тестовый кандидат\nТестируем новый карьерный сайт"
+				],
+				"auth_type" => "NATIVE"
+			]
+		]
+	);
 
-// 	echo $result;
-// });
+	$xxxx = json_encode($params);
+
+	$headers = array('Content-Type: application/json', 'Authorization: Bearer 66233397cc55c4714d52a78821c92910634e369cc3abee006304ccf6fb887097');
+
+	$ch = curl_init();
+	curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+	curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
+	curl_setopt($ch, CURLOPT_HTTPHEADER, $headers); // отправка заголовков
+	curl_setopt($ch, CURLOPT_POST, true);
+	curl_setopt($ch, CURLOPT_POSTFIELDS, $xxxx); // POST-данные
+	curl_setopt($ch, CURLOPT_URL, $url);
+	$result = curl_exec($ch);  
+	curl_close($ch);
+	// echo $result;
+
+});
