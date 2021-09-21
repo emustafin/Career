37f0f2d027ee0cb176987c73d8c017c5a7b3ac2e
Mel-SON <omelyanchenko.semen@yandex.ru>
2021-09-21 07:34:10 +0300

    Зачистка кода в файле direction.php

--- blocks/direction.php
+++ blocks/direction.php
@@ -16,29 +16,21 @@ $product_directions = new WP_Query( $args );
 
         <div class="custom-select" id="custom-select-first">
           <span class="custom-select__label">Выбери направление</span>
-          <span class="custom-select__change-select"><?php echo get_the_title($product_directions->posts[0]->ID); ?></span>
+          <span class="custom-select__change-select"><?= get_the_title($product_directions->posts[0]->ID); ?></span>
           <div class="custom-select__select-list-wrap">
             <ul class="custom-select__select-list">
-              <?php
-              $i = 1;
-              if ( $product_directions->have_posts() ) {
-                while ( $product_directions->have_posts() ) {
-                  $product_directions->the_post();
-//                   if( 1 == $i ){
-//                     $active = 'direction__direction-item-active';
-//                   } else{
-//                     $active = '';
-//                   }
-                  ?>
-                  <li class="custom-select__select-list-item tab-list-item <?php echo $active; ?>" data-tab-uid="<?php the_ID(); ?>" data-value="<?php the_title(); ?>">
-                    <?php the_title(); ?>
-                    <svg class="custom-select__list-icon" width="16" height="16" fill="none" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" clip-rule="evenodd" d="M6.5 10.151l5.576-5.575.849.848L6.5 11.848 3.076 8.425l.849-.848L6.5 10.15z" fill="#000"/></svg>
+              <? if ( $product_directions->have_posts() ):
+                while ( $product_directions->have_posts() ): $product_directions->the_post(); ?>
+
+                  <li class="custom-select__select-list-item tab-list-item <?= $active; ?>" data-tab-uid="<? the_ID(); ?>" data-value="<? the_title(); ?>">
+                    <? the_title(); ?>
+                    <svg class="custom-select__list-icon" width="16" height="16" fill="none" xmlns="http://www.w3.org/2000/svg">
+                      <path fill-rule="evenodd" clip-rule="evenodd" d="M6.5 10.151l5.576-5.575.849.848L6.5 11.848 3.076 8.425l.849-.848L6.5 10.15z" fill="#000"/>
+                    </svg>
                   </li>
-                  <?php
-                  $i++;
-                }
-              }
-              ?>
+                  
+                  <? endwhile;
+              endif;?>
             </ul>
           </div>
         </div>
@@ -51,43 +43,34 @@ $product_directions = new WP_Query( $args );
 
             <div class="direction__direction-container tab-list" id="tabController">
               <p class="direction__choose-mobile">Выбери направление</p>
-              <?php
-              $i = 1;
-              if ( $product_directions->have_posts() ) {
-                while ( $product_directions->have_posts() ) {
+              <? $i = 1;
+              if ( $product_directions->have_posts() ):
+                while ( $product_directions->have_posts() ):
                   $product_directions->the_post();
-                  if( 1 == $i ){
-                    $active = 'direction__direction-item-active';
-                  } else{
-                    $active = '';
-                  }
-                  ?>
-                  <div class="direction__direction-item tab-list-item <?php echo $active; ?>" data-tab-uid="<?php the_ID(); ?>">
-                    <p class="direction__direction"><?php the_title(); ?></p>
+                  $active = '';
+                  if( 1 == $i ) $active = 'direction__direction-item-active'; ?>
+
+                  <div class="direction__direction-item tab-list-item <?= $active; ?>" data-tab-uid="<? the_ID(); ?>">
+                    <p class="direction__direction"><? the_title(); ?></p>
                   </div>
-                  <?php
-                  $i++;
-                }
-              }
-              ?>
+
+                  <? $i++;
+                endwhile;
+              endif; ?>
             </div>
           </div>
       <div class="direction__main-content" id="tab-content">
-          <?php
-            $j = 1;
-            if ( $product_directions->have_posts() ) {
-              while ( $product_directions->have_posts() ) {
+          <? $j = 1;
+            if ( $product_directions->have_posts() ):
+              while ( $product_directions->have_posts() ):
                 $product_directions->the_post();
-                if( 1 == $j ){
-                  $main_display = 'display: block;';
-                } else{
-                  $main_display = 'display: none;';
-                }
-                ?>
-                <div class="tab-item-content" data-tab-uid='<?php echo the_ID(); ?>' style="<?php echo $main_display; ?>" >
+                $main_display = 'display: none;';
+                if( 1 == $j )  $main_display = 'display: block;'; ?>
+
+                <div class="tab-item-content" data-tab-uid='<?= the_ID(); ?>' style="<?= $main_display; ?>" >
                   <div class="direction__tribe-description">
                     <div class="direction__choosed tab-choice">
-                      <p class="direction__choosed-item"><?php the_title(); ?></p>
+                      <p class="direction__choosed-item"><? the_title(); ?></p>
                     </div>
 
                     <div class="direction__description-container">
@@ -95,32 +78,32 @@ $product_directions = new WP_Query( $args );
                         <div class="direction__director-block">
                           <div class="direction__director-image-block-mobile">
                             <img
-                              src="<?php echo get_the_post_thumbnail_url(); ?>"
+                              src="<?= get_the_post_thumbnail_url(); ?>"
                               alt="director"
                               class="direction__director-image"
                             />
                           </div>
                           <div class="direction__director-name-wrapper">
                             <p class="direction__director-name">
-                              <?php echo get_field( 'director_name' ); ?><span>,</span>
+                              <?= get_field( 'director_name' ); ?><span>,</span>
                             </p>
-                            <p class="direction__director-name"><?php echo get_field( 'director_position' ); ?></p>
+                            <p class="direction__director-name"><?= get_field( 'director_position' ); ?></p>
                           </div>
                         </div>
 
                         <div class="direction__director-block">
                           <div class="direction__director-image-block-mobile">
                             <img
-                              src="<?php echo get_the_post_thumbnail_url(); ?>"
+                              src="<?= get_the_post_thumbnail_url(); ?>"
                               alt="director"
                               class="direction__director-image"
                             />
                           </div>
                           <div class="direction__director-name-wrapper">
                             <p class="direction__director-name">
-                              <?php echo get_field( 'director_name' ); ?><span>,</span>
+                              <?= get_field( 'director_name' ); ?><span>,</span>
                             </p>
-                            <p class="direction__director-name"><?php echo get_field( 'director_position' ); ?></p>
+                            <p class="direction__director-name"><?= get_field( 'director_position' ); ?></p>
                           </div>
                         </div>
                       </div>
@@ -134,13 +117,10 @@ $product_directions = new WP_Query( $args );
 
                     <div class="direction__director-container">
 
-                      <?php
-                      if( have_rows('people') ):
-
+                      <? if( have_rows('people') ):
                         while( have_rows('people') ) : the_row();
-                    
-                            $sub_value = get_sub_field('sub_field');
-                            ?>
+                            $sub_value = get_sub_field('sub_field'); ?>
+
                             <div class="direction__director-block">
                               <div class="direction__director-image-block">
                                 <img
@@ -154,51 +134,51 @@ $product_directions = new WP_Query( $args );
                               </p>
                               <p class="direction__director-name"><?= get_sub_field('director_position'); ?></p>
                             </div>
-                            <?php
-                        endwhile;
-                      endif;
-                      ?>
 
+                        <? endwhile;
+                      endif; ?>
                     </div>
                   </div>
 
                   <!-- Блок с продуктами по направлениям -->
-                  <!-- <div class="direction__products-wrapper">
-                    <div class="direction__products-container">
-                      <p class="direction__products-title">Продукты трайба</p>
+                  <!-- 
+                    <div class="direction__products-wrapper">
+                      <div class="direction__products-container">
+                        <p class="direction__products-title">Продукты трайба</p>
+                      </div>
+
+                      <div class="direction__products-table-wrapper">
+                        <?php
+                        if( have_rows('direction_products') ):
+
+                            while( have_rows('direction_products') ) : the_row();
+
+                                ?>
+                                <div class="direction__products-table-item">
+                                  <p class="direction__item-title"><?= get_sub_field('title'); ?></p>
+                                  <p class="direction__item-text"><?= get_sub_field('description'); ?></p>
+                                </div>
+                                <?php
+
+                            endwhile;
+                        endif;
+                        ?>
+                      </div>
+
                     </div>
-
-                    <div class="direction__products-table-wrapper">
-                      <?php
-                      if( have_rows('direction_products') ):
-
-                          while( have_rows('direction_products') ) : the_row();
-
-                              ?>
-                              <div class="direction__products-table-item">
-                                <p class="direction__item-title"><?php echo get_sub_field('title'); ?></p>
-                                <p class="direction__item-text"><?php echo get_sub_field('description'); ?></p>
-                              </div>
-                              <?php
-
-                          endwhile;
-                      endif;
-                      ?>
-                    </div>
-
-                  </div>
-                  <a href="#" class="direction__products-link-mobile">
-                    еще
-                    <span class="direction__products-link-mobile-number">3</span>
-                    продукта
-                  </a> -->
+                    <a href="#" class="direction__products-link-mobile">
+                      еще
+                      <span class="direction__products-link-mobile-number">3</span>
+                      продукта
+                    </a> 
+                  -->
                   <!-- //Блок с продуктами по направлениям -->
 
                 </div>
-                <?php
-                $j++;
-              }
-            }
+
+                <? $j++;
+              endwhile;
+            endif;
             wp_reset_postdata();
           ?>
             </div>
