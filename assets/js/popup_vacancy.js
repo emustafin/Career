2112b706aaf3326e47e161847f1a541d0c27afa9
Mel-SON <omelyanchenko.semen@yandex.ru>
2021-10-20 03:21:05 +0300

    Меняет логику вывода инфориации в попап вакансии на аякс подгрузку

--- assets/js/popup_vacancy.js
+++ assets/js/popup_vacancy.js
@@ -1,31 +1,47 @@
 $('.profession__title').attr('originhref', window.location.href);
-$('.profession__job-wrapper').on('click', function (e) {
-  // var vacancy_info = $(this).attr('data-info');
-  // vacancy_info = jQuery.parseJSON(vacancy_info);
-
-  let vacancy_info;
-
-  if (e.target.classList.contains('profession__job-item')) {
-    vacancy_info = JSON.parse(e.target.dataset.info);
-  }
-
+$('.profession__job-item').on('click', function (e) {
+  
+  $('#vacancy_popup .loader-wrapp').css( 'display','block' );
   var origin_location = window.location.href;
 
-  $('.flyout .vacancy__headline-title').attr('origin_url', origin_location);
-  $('.flyout .vacancy__headline-title').html(vacancy_info.title);
-  $('.flyout .vacancy__video-container img').attr('src', vacancy_info.img);
-  $('.flyout .vacancy__intro-description').html(vacancy_info.content);
-  $('.flyout #vacancy_project').html(vacancy_info.vacancy_project);
-  $('.flyout #expectations').html(vacancy_info.expectations);
-  $('.flyout .vacancy__office-map-image').attr('src', vacancy_info.img_map);
-  $('.flyout .vacancy__office-adress-text').html(vacancy_info.map_full_adress);
-  $('.flyout .vacancy__meta-info-sum').html(vacancy_info.money_from);
-  $('.flyout .vacancy__meta-info-sum-after').html(vacancy_info.gross);
-  $('.flyout #vaccat_info_vacancy').html(vacancy_info.vaccat);
-  $('.flyout #town_info_vacancy').html(vacancy_info.town);
-  $('.flyout #experience_important').html(vacancy_info.experience);
+  post_id = $(this).attr('data-vacancy_id');
 
-  window.history.pushState('', '', vacancy_info.url);
+  var data = {
+      action: 'get_vacancy_data',
+      post_id : post_id
+  };
+
+  $.ajax({
+      type: 'POST',
+      url: ajax.url,
+      data: data,
+      dataType: 'json',
+      cache: 'false',
+      success: function (response) {
+          if( true == response.success ){
+
+            $('.flyout .vacancy__headline-title').attr('origin_url', origin_location);
+            $('.flyout .vacancy__headline-title').html(response.title);
+            $('.flyout .vacancy__video-container img').attr('src', response.img);
+            $('.flyout .vacancy__intro-description').html(response.content);
+            $('.flyout #vacancy_project').html(response.vacancy_project);
+            $('.flyout #expectations').html(response.expectations);
+            $('.flyout #can_work_remotely').html(response.can_work_remotely);
+            $('.flyout .vacancy__office-map-image').attr('src', response.img_map);
+            $('.flyout .vacancy__office-adress-text').html(response.map_full_adress);
+            $('.flyout .vacancy__meta-info-sum').html(response.money_from);
+            $('.flyout .vacancy__meta-info-sum-after').html(response.gross);
+            $('.flyout #vaccat_info_vacancy').html(response.vaccat);
+            $('.flyout #town_info_vacancy').html(response.town);
+            $('.flyout #experience_important').html(response.experience);
+
+            $('#vacancy_popup .loader-wrapp').css( 'display','none' );
+          
+            window.history.pushState('', '', response.url);
+
+          }
+      },
+  });
 });
 
 $('.vacancy__header-head-link-main').on('click', function (e) {
