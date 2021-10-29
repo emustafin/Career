755d1b1e207417301247977c7a54a094a66faddc
Mel-SON <omelyanchenko.semen@yandex.ru>
2021-10-29 07:54:02 +0300

    Натяжка страницы розницы

---
+++ templates/page-retail.php
@@ -0,0 +1,57 @@
+<?php 
+/*
+Template Name: Retail
+*/
+?>
+<?php get_header(); ?>
+
+<?php
+
+if( have_rows('content_blocks') ):
+  while ( have_rows('content_blocks') ) : the_row();
+
+    switch ( get_row_layout() ) {
+
+      case 'retail__preview':
+        include(THEME_DIR . '/template-parts/retail__preview.php');
+        break;
+
+      case 'retail__mission':
+        include(THEME_DIR . '/template-parts/retail__mission.php');
+        break;
+
+      case 'retail__why-us':
+        include(THEME_DIR . '/template-parts/retail__why-us.php');
+        break;
+
+      case 'brands':
+        include(THEME_DIR . '/template-parts/retail__brands.php');
+        break;
+
+      case 'retail__position':
+        include(THEME_DIR . '/template-parts/retail__position.php');
+        break;
+
+      case 'retail__quote':
+        include(THEME_DIR . '/template-parts/retail__quote.php');
+        break;
+
+      case 'retail__internship':
+        include(THEME_DIR . '/template-parts/retail__internship.php');
+        break;
+
+      case 'retail__advantages':
+        include(THEME_DIR . '/template-parts/retail__advantages.php');
+        break;
+
+      case 'retail__upgrade':
+        include(THEME_DIR . '/template-parts/upgrade.php');
+        break;
+
+    }
+
+  endwhile;
+endif;
+?>
+
+<?php get_footer(); ?>
