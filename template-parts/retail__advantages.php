4dcc10dd2c9489622cf68a729835ab3e01c31171
Mel-SON <omelyanchenko.semen@yandex.ru>
2021-11-10 01:39:32 +0300

    Перенатяжка с исправлением багов блока retail__advantages

--- template-parts/retail__advantages.php
+++ template-parts/retail__advantages.php
@@ -1,4 +1,8 @@
 <!-- Section advantages -->
+<?php
+$mvideo_retail_advantages = get_sub_field('mvideo_retail_advantages');
+$eldorado_retail_advantages = get_sub_field('eldorado_retail_advantages');
+?>
 <div class="retail__advantages">
   <div class="retail__advantages-side-bar">
     <p class="retail__advantages-side-bar-text">
@@ -6,7 +10,7 @@
     </p>
   </div>
 
-  <div class="retail__advantages-content-bar">
+  <!-- <div class="retail__advantages-content-bar">
     <div class="retail__advantages-content-item">
       <div class="retail__advantages-number">01</div>
       <div class="retail__advantages-description-block">
@@ -42,92 +46,82 @@
         </p>
       </div>
     </div>
-  </div>
+  </div> -->
 
   <div class="retail__advantages-content-bar mvideo">
-        <div class="retail__advantages-content-item">
-          <div class="retail__advantages-number-mvideo">01</div>
-          <div class="retail__advantages-description-block">
-            <h2 class="retail__advantages-description-title">
-              Современные технологии
-            </h2>
-            <p class="retail__advantages-description-text">
-              И это не пустые слова — мы уже внедрили Data Science
-              в планирование логистики и закупок и работаем над цифровизацией и
-              персонализацией покупательского опыта в магазинах.
-            </p>
-          </div>
-        </div>
-
-        <div class="retail__advantages-content-item">
-          <div class="retail__advantages-number-mvideo">02</div>
-          <div class="retail__advantages-description-block">
-            <h2 class="retail__advantages-description-title">
-              Обучение и развитие
-            </h2>
-            <p class="retail__advantages-description-text">
-              Мы проводим внутренние конкурсы и воркшопы и поощряем любые
-              инициативы сотрудников. У каждого человека в команде есть свой
-              индивидуальный план развития.
-            </p>
-          </div>
-        </div>
-
-        <div class="retail__advantages-content-item">
-          <div class="retail__advantages-number-mvideo">03</div>
-          <div class="retail__advantages-description-block">
-            <h2 class="retail__advantages-description-title">
-              Забота о сотрудниках
-            </h2>
-            <p class="retail__advantages-description-text">
-              Только официальное оформление и белая зарплата, гибкие программы
-              социальной поддержки, корпоративные скидки у партнеров.
-            </p>
-          </div>
+    <div class="retail__advantages-content-item">
+      <div class="retail__advantages-number-mvideo">01</div>
+      <div class="retail__advantages-description-block">
+        <h2 class="retail__advantages-description-title">
+          <?php echo $mvideo_retail_advantages['reason_header_1']; ?>
+        </h2>
+        <div class="retail__advantages-description-text">
+          <?php echo $mvideo_retail_advantages['context_reason_1']; ?>
         </div>
       </div>
+    </div>
 
-      <div class="retail__advantages-content-bar eldorado hide">
-        <div class="retail__advantages-content-item">
-          <div class="retail__advantages-number-eldorado">01</div>
-          <div class="retail__advantages-description-block">
-            <h2 class="retail__advantages-description-title">
-              Современные технологии
-            </h2>
-            <p class="retail__advantages-description-text">
-              И это не пустые слова — мы уже внедрили Data Science
-              в планирование логистики и закупок и работаем над цифровизацией и
-              персонализацией покупательского опыта в магазинах.
-            </p>
-          </div>
-        </div>
-
-        <div class="retail__advantages-content-item">
-          <div class="retail__advantages-number-eldorado">02</div>
-          <div class="retail__advantages-description-block">
-            <h2 class="retail__advantages-description-title">
-              Обучение и развитие
-            </h2>
-            <p class="retail__advantages-description-text">
-              Мы проводим внутренние конкурсы и воркшопы и поощряем любые
-              инициативы сотрудников. У каждого человека в команде есть свой
-              индивидуальный план развития.
-            </p>
-          </div>
-        </div>
-
-        <div class="retail__advantages-content-item">
-          <div class="retail__advantages-number-eldorado">03</div>
-          <div class="retail__advantages-description-block">
-            <h2 class="retail__advantages-description-title">
-              Забота о сотрудниках
-            </h2>
-            <p class="retail__advantages-description-text">
-              Только официальное оформление и белая зарплата, гибкие программы
-              социальной поддержки, корпоративные скидки у партнеров.
-            </p>
-          </div>
+    <div class="retail__advantages-content-item">
+      <div class="retail__advantages-number-mvideo">02</div>
+      <div class="retail__advantages-description-block">
+        <h2 class="retail__advantages-description-title">
+          <?php echo $mvideo_retail_advantages['reason_header_2']; ?>
+        </h2>
+        <div class="retail__advantages-description-text">
+          <?php echo $mvideo_retail_advantages['context_reason_2']; ?>
         </div>
       </div>
+    </div>
+
+    <div class="retail__advantages-content-item">
+      <div class="retail__advantages-number-mvideo">03</div>
+      <div class="retail__advantages-description-block">
+        <h2 class="retail__advantages-description-title">
+          <?php echo $mvideo_retail_advantages['reason_header_3']; ?>
+        </h2>
+        <div class="retail__advantages-description-text">
+          <?php echo $mvideo_retail_advantages['context_reason_3']; ?>
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <div class="retail__advantages-content-bar eldorado hide">
+    <div class="retail__advantages-content-item">
+      <div class="retail__advantages-number-eldorado">01</div>
+      <div class="retail__advantages-description-block">
+        <h2 class="retail__advantages-description-title">
+          <?php echo $eldorado_retail_advantages['reason_header_2']; ?>
+        </h2>
+        <div class="retail__advantages-description-text">
+          <?php echo $eldorado_retail_advantages['context_reason_1']; ?>
+        </div>
+      </div>
+    </div>
+
+    <div class="retail__advantages-content-item">
+      <div class="retail__advantages-number-eldorado">02</div>
+      <div class="retail__advantages-description-block">
+        <h2 class="retail__advantages-description-title">
+          <?php echo $eldorado_retail_advantages['reason_header_2']; ?>
+        </h2>
+        <div class="retail__advantages-description-text">
+          <?php echo $eldorado_retail_advantages['context_reason_2']; ?>
+        </div>
+      </div>
+    </div>
+
+    <div class="retail__advantages-content-item">
+      <div class="retail__advantages-number-eldorado">03</div>
+      <div class="retail__advantages-description-block">
+        <h2 class="retail__advantages-description-title">
+          <?php echo $eldorado_retail_advantages['reason_header_2']; ?>
+        </h2>
+        <div class="retail__advantages-description-text">
+          <?php echo $eldorado_retail_advantages['context_reason_3']; ?>
+        </div>
+      </div>
+    </div>
+  </div>
 </div>
 <!-- //Section advantages -->
