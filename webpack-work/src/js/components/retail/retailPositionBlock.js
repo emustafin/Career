0cf701f4d6773abddd3256cef7c367ca999a2be2
Vladimir <shadrov_vladimir@mail.ru>
2021-11-11 19:08:32 +0300

    Bug/Добавлено открытие попапа вакансии при клике на вакансии. Исправлена обрезка кнопки "больше о направлении"

--- webpack-work/src/js/components/retail/retailPositionBlock.js
+++ webpack-work/src/js/components/retail/retailPositionBlock.js
@@ -12,16 +12,31 @@ export class RetailPositionBlock {
       this.el.querySelectorAll('.eldorado')
     );
     this.vacancyItems = this.el.querySelectorAll('.retail__position-list-item');
+    this.flyout = document.querySelector('.flyout');
+    this.flyOutContentBar = this.flyout.querySelector('.vacancy');
+    this.flyoutSideBar = this.flyout.querySelector('.flyout__side-bar');
 
     this.el.addEventListener('mouseover', this.getVacancyItems.bind(this));
 
+    //--------------------------------------------------------------------------------------------
+    // Скрипт копирует ссылку на страницу вакансии и открывает ее.
+
+    // this.vacancyItems.forEach((vacancy) => {
+    //   vacancy.addEventListener('click', () => {
+    //     const button = vacancy.querySelector('.retail__position-item-button');
+    //     const linkAddress = button.href;
+    //     document.location.href = linkAddress;
+    //   });
+    // });
+
+    //-----------------------------------------------------------------------------------------------
+    // Скрипт для открытия попапа вакансии при клике на блоке вакансии
+
     this.vacancyItems.forEach((vacancy) => {
-      vacancy.addEventListener('click', () => {
-        const button = vacancy.querySelector('.retail__position-item-button');
-        const linkAddress = button.href;
-        document.location.href = linkAddress;
-      });
+      vacancy.addEventListener('click', this.openFlyout.bind(this));
     });
+
+    //---------------------------------------------------------------------------------------------
   }
   getVacancyItems(event) {
     if (event.target.classList.contains('retail__position-list-item')) {
@@ -42,5 +57,18 @@ export class RetailPositionBlock {
         button.style.left = event.layerX - 30 + 'px';
       });
     });
+  }
+
+  openFlyout() {
+    this.flyout.classList.add('flyout__active');
+    this.flyOutContentBar.scrollTop = 0;
+
+    setTimeout(() => {
+      document.body.style.overflow = 'hidden';
+    }, 200);
+
+    setTimeout(() => {
+      this.flyoutSideBar.style.background = 'rgba(0, 0, 0, 0.8)';
+    }, 300);
   }
 }
