682d6c41b81cbc16887277dc7770b0585b065d6b
Mel-SON <omelyanchenko.semen@yandex.ru>
2021-12-24 01:28:16 +0300

    Натяжка страницы Логистики (без вакасний)

---
+++ template-parts/logistic/career_screen.php
@@ -0,0 +1,328 @@
+<!-- Logistic Career -->
+<section class="logistic-career">
+  <div class="retail__position-title-wrapper eldorado">
+    <h2 class="retail__position-title"><?php echo get_sub_field('title'); ?></h2>
+  </div>
+
+  <div class="retail__position-content eldorado">
+    <div class="retail__position-side-bar">
+      <p class="retail__position-side-bar-title">Начать карьеру просто</p>
+
+      <div class="retail__position-side-bar-item">
+        <h3 class="retail__position-side-bar-item-title-eldorado">01</h3>
+        <p class="retail__position-side-bar-text"><?php echo get_sub_field('text_step_1'); ?></p>
+      </div>
+
+      <div class="retail__position-side-bar-item">
+        <h3 class="retail__position-side-bar-item-title-eldorado">02</h3>
+        <p class="retail__position-side-bar-text"><?php echo get_sub_field('text_step_2'); ?></p>
+      </div>
+
+      <div class="retail__position-side-bar-item">
+        <h3 class="retail__position-side-bar-item-title-eldorado">03</h3>
+        <p class="retail__position-side-bar-text"><?php echo get_sub_field('text_step_3'); ?></p>
+      </div>
+    </div>
+
+    <div class="retail__position-content-bar">
+      <div class="retail__position-headline">
+        <p class="retail__position-headline-text">Наши вакансии</p>
+        <!-- <a href="#" class="retail__position-headline-link">
+          Все вакансии
+
+          <svg
+            class="retail__position-headline-link-arrow"
+            width="14"
+            height="10"
+            viewBox="0 0 14 10"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              d="M11.5515 4.40044L8.57574 1.4247L9.42426 0.576172L13.8485 5.00044L9.42426 9.4247L8.57574 8.57617L11.5515 5.60044H0V4.40044H11.5515Z"
+              fill="black"
+            />
+          </svg>
+        </a> -->
+      </div>
+
+      <div class="retail__position-list">
+        <div class="retail__position-list-item">
+          <div class="retail__position-item-position">
+            <p class="retail__position-item-position-title">Кладовщик</p>
+          </div>
+
+          <div class="retail__position-item-description">
+            <p class="retail__position-description-text">
+              Главный человек в компании! Правая рука покупателя, может
+              подобрать идеальный гаджет одной левой.
+            </p>
+
+            <a
+              href="#"
+              class="
+                retail__position-description-link
+                logistic-career__position-description-link
+              "
+            >
+              <span class="retail__position-value">343</span>
+              вакансии
+              <svg
+                class="retail__position-headline-link-arrow"
+                width="14"
+                height="10"
+                viewBox="0 0 14 10"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  d="M11.5515 4.40044L8.57574 1.4247L9.42426 0.576172L13.8485 5.00044L9.42426 9.4247L8.57574 8.57617L11.5515 5.60044H0V4.40044H11.5515Z"
+                  fill="black"
+                />
+              </svg>
+            </a>
+          </div>
+          <a href="#" class="retail__position-item-button">
+            больше о направлении
+
+            <svg
+              width="14"
+              height="10"
+              viewBox="0 0 14 10"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                d="M11.5515 4.40044L8.57574 1.4247L9.42426 0.576172L13.8485 5.00044L9.42426 9.4247L8.57574 8.57617L11.5515 5.60044H0V4.40044H11.5515Z"
+                fill="black"
+              />
+            </svg>
+          </a>
+        </div>
+
+        <div class="retail__position-list-item">
+          <div class="retail__position-item-position">
+            <p class="retail__position-item-position-title">
+              Комплектовщик
+            </p>
+          </div>
+
+          <div class="retail__position-item-description">
+            <p class="retail__position-description-text">
+              Главный человек в компании! Правая рука покупателя, может
+              подобрать идеальный гаджет одной левой.
+            </p>
+
+            <a
+              href="#"
+              class="
+                retail__position-description-link
+                logistic-career__position-description-link
+              "
+            >
+              <span class="retail__position-value">343</span>
+              вакансии
+              <svg
+                class="retail__position-headline-link-arrow"
+                width="14"
+                height="10"
+                viewBox="0 0 14 10"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  d="M11.5515 4.40044L8.57574 1.4247L9.42426 0.576172L13.8485 5.00044L9.42426 9.4247L8.57574 8.57617L11.5515 5.60044H0V4.40044H11.5515Z"
+                  fill="black"
+                />
+              </svg>
+            </a>
+          </div>
+          <a href="#" class="retail__position-item-button">
+            больше о направлении
+
+            <svg
+              width="14"
+              height="10"
+              viewBox="0 0 14 10"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                d="M11.5515 4.40044L8.57574 1.4247L9.42426 0.576172L13.8485 5.00044L9.42426 9.4247L8.57574 8.57617L11.5515 5.60044H0V4.40044H11.5515Z"
+                fill="black"
+              />
+            </svg>
+          </a>
+        </div>
+
+        <div class="retail__position-list-item">
+          <div class="retail__position-item-position">
+            <p class="retail__position-item-position-title">
+              Водитель погрузчика
+            </p>
+          </div>
+
+          <div class="retail__position-item-description">
+            <p class="retail__position-description-text">
+              Главный человек в компании! Правая рука покупателя, может
+              подобрать идеальный гаджет одной левой.
+            </p>
+
+            <a
+              href="#"
+              class="
+                retail__position-description-link
+                logistic-career__position-description-link
+              "
+            >
+              <span class="retail__position-value">343</span>
+              вакансии
+              <svg
+                class="retail__position-headline-link-arrow"
+                width="14"
+                height="10"
+                viewBox="0 0 14 10"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  d="M11.5515 4.40044L8.57574 1.4247L9.42426 0.576172L13.8485 5.00044L9.42426 9.4247L8.57574 8.57617L11.5515 5.60044H0V4.40044H11.5515Z"
+                  fill="black"
+                />
+              </svg>
+            </a>
+          </div>
+          <a href="#" class="retail__position-item-button">
+            больше о направлении
+
+            <svg
+              width="14"
+              height="10"
+              viewBox="0 0 14 10"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                d="M11.5515 4.40044L8.57574 1.4247L9.42426 0.576172L13.8485 5.00044L9.42426 9.4247L8.57574 8.57617L11.5515 5.60044H0V4.40044H11.5515Z"
+                fill="black"
+              />
+            </svg>
+          </a>
+        </div>
+
+        <div class="retail__position-list-item">
+          <div class="retail__position-item-position">
+            <p class="retail__position-item-position-title">
+              Водитель погрузчика
+            </p>
+          </div>
+
+          <div class="retail__position-item-description">
+            <p class="retail__position-description-text">
+              Главный человек в компании! Правая рука покупателя, может
+              подобрать идеальный гаджет одной левой.
+            </p>
+
+            <a
+              href="#"
+              class="
+                retail__position-description-link
+                logistic-career__position-description-link
+              "
+            >
+              <span class="retail__position-value">343</span>
+              вакансии
+              <svg
+                class="retail__position-headline-link-arrow"
+                width="14"
+                height="10"
+                viewBox="0 0 14 10"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  d="M11.5515 4.40044L8.57574 1.4247L9.42426 0.576172L13.8485 5.00044L9.42426 9.4247L8.57574 8.57617L11.5515 5.60044H0V4.40044H11.5515Z"
+                  fill="black"
+                />
+              </svg>
+            </a>
+          </div>
+          <a href="#" class="retail__position-item-button">
+            больше о направлении
+
+            <svg
+              width="14"
+              height="10"
+              viewBox="0 0 14 10"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                d="M11.5515 4.40044L8.57574 1.4247L9.42426 0.576172L13.8485 5.00044L9.42426 9.4247L8.57574 8.57617L11.5515 5.60044H0V4.40044H11.5515Z"
+                fill="black"
+              />
+            </svg>
+          </a>
+        </div>
+
+        <div class="retail__position-list-item">
+          <div class="retail__position-item-position">
+            <p class="retail__position-item-position-title">
+              Водитель погрузчика
+            </p>
+          </div>
+
+          <div class="retail__position-item-description">
+            <p class="retail__position-description-text">
+              Главный человек в компании! Правая рука покупателя, может
+              подобрать идеальный гаджет одной левой.
+            </p>
+
+            <a
+              href="#"
+              class="
+                retail__position-description-link
+                logistic-career__position-description-link
+              "
+            >
+              <span class="retail__position-value">343</span>
+              вакансии
+              <svg
+                class="retail__position-headline-link-arrow"
+                width="14"
+                height="10"
+                viewBox="0 0 14 10"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  d="M11.5515 4.40044L8.57574 1.4247L9.42426 0.576172L13.8485 5.00044L9.42426 9.4247L8.57574 8.57617L11.5515 5.60044H0V4.40044H11.5515Z"
+                  fill="black"
+                />
+              </svg>
+            </a>
+          </div>
+          <a href="#" class="retail__position-item-button">
+            больше о направлении
+
+            <svg
+              width="14"
+              height="10"
+              viewBox="0 0 14 10"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                d="M11.5515 4.40044L8.57574 1.4247L9.42426 0.576172L13.8485 5.00044L9.42426 9.4247L8.57574 8.57617L11.5515 5.60044H0V4.40044H11.5515Z"
+                fill="black"
+              />
+            </svg>
+          </a>
+        </div>
+      </div>
+
+      <!-- <button class="logistic-career__button">показать ещё +</button> -->
+    </div>
+  </div>
+</section>
+<!-- //Logistic Career -->
