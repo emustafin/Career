16eca804f4259b9ef6f170b70839c6670041eb1f
Mel-SON <omelyanchenko.semen@yandex.ru>
2021-12-24 03:20:44 +0300

    Натяжка страницы офиса используя уже готовые блоки

---
+++ templates/page-office.php
@@ -0,0 +1,57 @@
+<?php 
+/*
+Template Name: Office
+*/
+?>
+<?php get_header(); ?>
+<script>
+    var rel_type = 'office';
+    var vacancyid = '';
+    var sourceurl = '';
+</script>
+<?php
+
+if( have_rows('office_blocks') ):
+  while ( have_rows('office_blocks') ) : the_row();
+
+    switch ( get_row_layout() ) {
+
+      case 'main_screen':
+        include(THEME_DIR . '/template-parts/office/main_screen.php');
+        break;
+
+      case 'why-are-we':
+        include(THEME_DIR . '/template-parts/it/why-are-we.php');
+        break;
+
+      case 'profession':
+        include(THEME_DIR . '/template-parts/it/profession.php');
+        break;
+
+      case 'reason':
+        include(THEME_DIR . '/template-parts/logistic/why-we-are.php');
+        break;
+
+      case 'upgrade_screen':
+        $relation = 'office';
+        include(THEME_DIR . '/template-parts/common/upgrade.php');
+        break;
+
+      case 'running_line':
+        include(THEME_DIR . '/template-parts/common/running-line.php');
+        break;
+
+      case 'quote':
+        include(THEME_DIR . '/template-parts/logistic/quote_screen.php');
+        break;
+
+      case 'intern':
+        include(THEME_DIR . '/template-parts/common/intern.php');
+        break;
+    }
+
+  endwhile;
+endif;
+?>
+
+<?php get_footer(); ?>
