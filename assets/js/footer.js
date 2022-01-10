b644184e5759a4f60a315b926b5b318911cbab7d
kosmich02144 <Pa9buqyz>
2022-01-10 09:33:25 +0300

    Переписывает jquery на нативный js

--- assets/js/footer.js
+++ assets/js/footer.js
@@ -1,12 +1,14 @@
-document.querySelector('#popup_form .wpcf7-submit').onclick = function(e){
+document.querySelector('#popup_form .wpcf7-submit').addEventListener("click", function(e) {
   document.querySelector("#popup_form .vacancy__form-label_checkbox").style.color = "#000000";
   document.querySelector("#popup_form  .vacancy__form-label_checkbox a").style.color = "#000000";
-};
+  document.querySelector("#popup_form  .vacancy__form-label_checkbox a").style.borderBottom = "#000000 1px solid";
+}, false);
 
-document.querySelector('#vacancy_form .wpcf7-submit').onclick = function(e){
+document.querySelector('#vacancy_form .wpcf7-submit').addEventListener("click", function(e) {
   document.querySelector("#vacancy_form .vacancy__form-label_checkbox").style.color = "#000000";
   document.querySelector("#vacancy_form  .vacancy__form-label_checkbox a").style.color = "#000000";
-};
+  document.querySelector("#vacancy_form  .vacancy__form-label_checkbox a").style.borderBottom = "#000000 1px solid";
+}, false);
 
 // Отправка анкеты на сервер
 const form = document.querySelector('#popup_form .wpcf7');
@@ -32,6 +34,7 @@ form.addEventListener(
           var label_checkbox_a = document.querySelector('.form__content-right-bar .vacancy__form-label_checkbox a');
           label_checkbox.style.color = '#e31235';
           label_checkbox_a.style.color = '#e31235';
+          label_checkbox_a.style.borderBottom = "#e31235 1px solid";
         }
       }
       return;
@@ -63,6 +66,7 @@ form2.addEventListener(
           let label_checkbox_a = document.querySelector('#vacancy_form .vacancy__form-label_checkbox a');
           label_checkbox.style.color = '#e31235';
           label_checkbox_a.style.color = '#e31235';
+          label_checkbox_a.style.borderBottom = "#e31235 1px solid";
         }
       }
       return;
@@ -173,17 +177,18 @@ spezialisationSelect.on(
 
 function setup_vars_for_forms() {
 
-  document.getElementsByClassName('rel_type').forEach(
+  document.querySelectorAll('rel_type').forEach(
     element => element.setAttribute('value',rel_type),
   );
 
-  document.getElementsByClassName('vacancyid').forEach(
+  document.querySelectorAll('vacancyid').forEach(
     element => element.setAttribute('value',vacancyid),
   );
 
-  document.getElementsByClassName('sourceurl').forEach(
+  document.querySelectorAll('sourceurl').forEach(
     element => element.setAttribute('value',sourceurl),
   );
 
-}
+};
+
 setup_vars_for_forms();
