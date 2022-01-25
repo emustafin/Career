e2540915a0ab347ec0731c3c2aee0c7f2453cb9c
Mel-SON <omelyanchenko.semen@yandex.ru>
2022-01-25 15:37:05 +0300

    Добавляет проверку во избежания ошибки 2

--- template-parts/common/upgrade.php
+++ template-parts/common/upgrade.php
@@ -247,30 +247,14 @@ if( !is_front_page() ){
 
             <div class="upgrade__slider-wrapper">
 
-                <div class="slider" id="slider-main-eldorado" data-slider-id="slider-left-eldorado slider-main-eldorado slider-right-eldorado">
-                    <div class="slider__btn slider slider__left" id="slider-left-eldorado"  data-slider-id="slider-left-eldorado slider-main-eldorado slider-right-eldorado" data-slider-side="right">
-                        <div class="slider__list slider__list_m-0 slider__list_w-100 slider__list_h-100">
+                <?php if ( $reviews->have_posts() ) { ?>
+                    <div class="slider" id="slider-main-eldorado" data-slider-id="slider-left-eldorado slider-main-eldorado slider-right-eldorado">
+                        <div class="slider__btn slider slider__left" id="slider-left-eldorado"  data-slider-id="slider-left-eldorado slider-main-eldorado slider-right-eldorado" data-slider-side="right">
+                            <div class="slider__list slider__list_m-0 slider__list_w-100 slider__list_h-100">
 
-                            <?php
-                            if ( $reviews->have_posts() ) {
-                                $post = $reviews->posts[count($reviews->posts)-1];
-                                $thumbnail_url = get_the_post_thumbnail_url( $post->ID, 'full' );
-                                if( false == $thumbnail_url ){
-                                    $thumbnail_url = THEME_URL .'/assets/images/default/post-image-default.jpg';
-                                }
-                                ?>
-                                <div class="slider__item loading">
-                                    <img class="slider__left-img"
-                                        data-src="<?php echo $thumbnail_url; ?>"
-                                        src="<?php echo THEME_URL; ?>/assets/images/Lazy-loading/1x1.png"
-                                        alt="Photo"
-                                        width="100"
-                                        height="100"
-                                    >
-                                </div>
                                 <?php
-                                for ($i=0; $i < count($reviews->posts)-1; $i++) {
-                                    $post = $reviews->posts[$i];
+                                if ( $reviews->have_posts() ) {
+                                    $post = $reviews->posts[count($reviews->posts)-1];
                                     $thumbnail_url = get_the_post_thumbnail_url( $post->ID, 'full' );
                                     if( false == $thumbnail_url ){
                                         $thumbnail_url = THEME_URL .'/assets/images/default/post-image-default.jpg';
@@ -286,97 +270,134 @@ if( !is_front_page() ){
                                         >
                                     </div>
                                     <?php
-                                }
-                            }
-                            ?>
-                        </div>
-                    </div>
-                    <div data-slider-id="slider-left-eldorado slider-main-eldorado slider-right-eldorado" data-slider-side="right" class="slider__btn slider__btn-left"></div>
-                    <div class="slider__list">
-
-                        <?php
-                        if ( $reviews->have_posts() ) {
-                            while ( $reviews->have_posts() ) {
-                                $reviews->the_post();
-
-                                $thumbnail_url = get_the_post_thumbnail_url( get_the_ID(), 'full' );
-                                if( false == $thumbnail_url ){
-                                    $thumbnail_url = THEME_URL .'/assets/images/default/post-image-default.jpg';
+                                    for ($i=0; $i < count($reviews->posts)-1; $i++) {
+                                        $post = $reviews->posts[$i];
+                                        $thumbnail_url = get_the_post_thumbnail_url( $post->ID, 'full' );
+                                        if( false == $thumbnail_url ){
+                                            $thumbnail_url = THEME_URL .'/assets/images/default/post-image-default.jpg';
+                                        }
+                                        ?>
+                                        <div class="slider__item loading">
+                                            <img class="slider__left-img"
+                                                data-src="<?php echo $thumbnail_url; ?>"
+                                                src="<?php echo THEME_URL; ?>/assets/images/Lazy-loading/1x1.png"
+                                                alt="Photo"
+                                                width="100"
+                                                height="100"
+                                            >
+                                        </div>
+                                        <?php
+                                    }
                                 }
                                 ?>
-                                <div class="slider__item">
-                                    <div class="slider__content">
-                                        <div class="slider__content-paragraph">
-                                            <div class="slider__info-people loading">
-                                                <img class="slider__img"
-                                                    data-src="<?php echo $thumbnail_url; ?>"
-                                                    src="<?php echo THEME_URL; ?>/assets/images/Lazy-loading/1x1.png"
-                                                    alt="Photo"
-                                                    title="Наши истории успеха"
-                                                    width="100"
-                                                    height="100"
-                                                >
-                                                <p class="slider__job">
-                                                    <span class="slider__job-name"><?php the_title(); ?></span>, 
-                                                    <br> 
-                                                    <span class="slider__job-profession">
-                                                        <?php echo get_field('proffesion'); ?>
-                                                    </span>
-                                                </p>
-                                            </div>
-                                            <p class="slider__main-text"><?php the_content(); ?></p>
-                                        </div>
-                                        <div class="slider__content-road">
-                                            <div class="slider__way">
-                                                <?php
-                                                $k = 1;
-                                                if( have_rows('career_way') ):
-
-                                                    while( have_rows('career_way') ) : the_row();
-
-                                                        ?>
-                                                        <div class="slider__way-item">
-                                                            <span class="slider__way-year"><?php echo get_sub_field('year'); ?></span>
-                                                            <span class="slider__way-profession"><?php echo get_sub_field('proff'); ?></span>
-                                                        </div>
-                                                        <?php
-                                                        $k++;
-                                                    endwhile;
-                                                endif;
-                                                ?>
-                                            </div>
-                                            <div class="slider__way-mobile">
-                                                <?php
-                                                if( have_rows('career_way') ):
-
-                                                    while( have_rows('career_way') ) : the_row();
-                                                        ?>
-                                                        <div class="slider__way-item-mobile">
-                                                            <span class="slider__way-year"><?php echo get_sub_field('year'); ?></span>&nbsp;<span class="slider__way-profession"><?php echo get_sub_field('proff'); ?></span>
-                                                        </div>
-                                                        <?php
-                                                    endwhile;
-                                                endif;
-                                                ?>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
-                                <?php
-                            }
-                        }
-                        ?>
-
-                    </div>
-                    <div data-slider-id="slider-left-eldorado slider-main-eldorado slider-right-eldorado" data-slider-side="left" class="slider__btn slider__btn-right"></div>
-                    <div data-slider-id="slider-left-eldorado slider-main-eldorado slider-right-eldorado" data-slider-side="left" class="slider__btn slider slider__right" id="slider-right-eldorado">
-                        <div class="slider__list slider__list_m-0 slider__list_2-100 slider__list_w_40px">
+                            </div>
+                        </div>
+                        <div data-slider-id="slider-left-eldorado slider-main-eldorado slider-right-eldorado" data-slider-side="right" class="slider__btn slider__btn-left"></div>
+                        <div class="slider__list">
 
                             <?php
                             if ( $reviews->have_posts() ) {
+                                while ( $reviews->have_posts() ) {
+                                    $reviews->the_post();
 
-                                for ($i=1; $i < count($reviews->posts); $i++) {
-                                    $post = $reviews->posts[$i];
+                                    $thumbnail_url = get_the_post_thumbnail_url( get_the_ID(), 'full' );
+                                    if( false == $thumbnail_url ){
+                                        $thumbnail_url = THEME_URL .'/assets/images/default/post-image-default.jpg';
+                                    }
+                                    ?>
+                                    <div class="slider__item">
+                                        <div class="slider__content">
+                                            <div class="slider__content-paragraph">
+                                                <div class="slider__info-people loading">
+                                                    <img class="slider__img"
+                                                        data-src="<?php echo $thumbnail_url; ?>"
+                                                        src="<?php echo THEME_URL; ?>/assets/images/Lazy-loading/1x1.png"
+                                                        alt="Photo"
+                                                        title="Наши истории успеха"
+                                                        width="100"
+                                                        height="100"
+                                                    >
+                                                    <p class="slider__job">
+                                                        <span class="slider__job-name"><?php the_title(); ?></span>, 
+                                                        <br> 
+                                                        <span class="slider__job-profession">
+                                                            <?php echo get_field('proffesion'); ?>
+                                                        </span>
+                                                    </p>
+                                                </div>
+                                                <p class="slider__main-text"><?php the_content(); ?></p>
+                                            </div>
+                                            <div class="slider__content-road">
+                                                <div class="slider__way">
+                                                    <?php
+                                                    $k = 1;
+                                                    if( have_rows('career_way') ):
+
+                                                        while( have_rows('career_way') ) : the_row();
+
+                                                            ?>
+                                                            <div class="slider__way-item">
+                                                                <span class="slider__way-year"><?php echo get_sub_field('year'); ?></span>
+                                                                <span class="slider__way-profession"><?php echo get_sub_field('proff'); ?></span>
+                                                            </div>
+                                                            <?php
+                                                            $k++;
+                                                        endwhile;
+                                                    endif;
+                                                    ?>
+                                                </div>
+                                                <div class="slider__way-mobile">
+                                                    <?php
+                                                    if( have_rows('career_way') ):
+
+                                                        while( have_rows('career_way') ) : the_row();
+                                                            ?>
+                                                            <div class="slider__way-item-mobile">
+                                                                <span class="slider__way-year"><?php echo get_sub_field('year'); ?></span>&nbsp;<span class="slider__way-profession"><?php echo get_sub_field('proff'); ?></span>
+                                                            </div>
+                                                            <?php
+                                                        endwhile;
+                                                    endif;
+                                                    ?>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <?php
+                                }
+                            }
+                            ?>
+
+                        </div>
+                        <div data-slider-id="slider-left-eldorado slider-main-eldorado slider-right-eldorado" data-slider-side="left" class="slider__btn slider__btn-right"></div>
+                        <div data-slider-id="slider-left-eldorado slider-main-eldorado slider-right-eldorado" data-slider-side="left" class="slider__btn slider slider__right" id="slider-right-eldorado">
+                            <div class="slider__list slider__list_m-0 slider__list_2-100 slider__list_w_40px">
+
+                                <?php
+                                if ( $reviews->have_posts() ) {
+
+                                    for ($i=1; $i < count($reviews->posts); $i++) {
+                                        $post = $reviews->posts[$i];
+                                        $thumbnail_url = get_the_post_thumbnail_url( $post->ID, 'full' );
+                                        if( false == $thumbnail_url ){
+                                            $thumbnail_url = THEME_URL .'/assets/images/default/post-image-default.jpg';
+                                        }
+                                        ?>
+                                        <div class="slider__item loading">
+                                            <img class="slider__right-img"
+                                                data-src="<?php echo $thumbnail_url; ?>"
+                                                src="<?php echo THEME_URL; ?>/assets/images/Lazy-loading/1x1.png"
+                                                alt="Image"
+                                                title="Наши истории успеха"
+                                                width="100"
+                                                height="100"
+                                            >
+                                            <p class="slider__right-name-and-position"><span class="slider__job-name"><?php echo get_the_title( $post->ID ); ?>	</span>, <br> <span class="slider__job-profession"><?php echo get_field('proffesion', $post->ID ); ?></span></p>
+                                        </div>
+                                        <?php
+                                    }
+
+                                    $post = $reviews->posts[0];
                                     $thumbnail_url = get_the_post_thumbnail_url( $post->ID, 'full' );
                                     if( false == $thumbnail_url ){
                                         $thumbnail_url = THEME_URL .'/assets/images/default/post-image-default.jpg';
@@ -395,32 +416,12 @@ if( !is_front_page() ){
                                     </div>
                                     <?php
                                 }
-
-                                $post = $reviews->posts[0];
-                                $thumbnail_url = get_the_post_thumbnail_url( $post->ID, 'full' );
-                                if( false == $thumbnail_url ){
-                                    $thumbnail_url = THEME_URL .'/assets/images/default/post-image-default.jpg';
-                                }
+                                wp_reset_postdata();
                                 ?>
-                                <div class="slider__item loading">
-                                    <img class="slider__right-img"
-                                        data-src="<?php echo $thumbnail_url; ?>"
-                                        src="<?php echo THEME_URL; ?>/assets/images/Lazy-loading/1x1.png"
-                                        alt="Image"
-                                        title="Наши истории успеха"
-                                        width="100"
-                                        height="100"
-                                    >
-                                    <p class="slider__right-name-and-position"><span class="slider__job-name"><?php echo get_the_title( $post->ID ); ?>	</span>, <br> <span class="slider__job-profession"><?php echo get_field('proffesion', $post->ID ); ?></span></p>
-                                </div>
-                                <?php
-                            }
-                            wp_reset_postdata();
-                            ?>
+                            </div>
                         </div>
                     </div>
-                </div>
-
+                <?php } ?>
             </div>
         </div>
 <?php } ?>
