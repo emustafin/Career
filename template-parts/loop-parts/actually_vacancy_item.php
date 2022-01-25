6573e3f0dafb5bd9d7ae9bfa722e4da237928951
Mel-SON <omelyanchenko.semen@yandex.ru>
2022-01-25 15:15:03 +0300

    Перенатяжка под актуальные переменные и пересборка форматирования для единой вакансии в списке

--- template-parts/loop-parts/actually_vacancy_item.php
+++ template-parts/loop-parts/actually_vacancy_item.php
@@ -1,8 +1,8 @@
 <?php
-$vaccat_terms = get_the_terms( $vacancy_item_id, 'vaccat' );
-if( is_array( $vaccat_terms ) ){
-	foreach( $vaccat_terms as $vaccat_term ){
-        $first_vaccat = $vaccat_term;
+$specialization_terms = get_the_terms( $vacancy_item_id, $_modificator.'specialization' );
+if( is_array( $specialization_terms ) ){
+	foreach( $specialization_terms as $specialization_term ){
+        $first_specialization = $specialization_term;
         break;
 	}
 }
@@ -16,88 +16,66 @@ if( is_array( $vaccat_terms ) ){
         <div class="profession__bread-crumbs">
             <a href="#" class="profession__crumb">
                 IT-департамент
-                <svg
-                        width="12"
-                        height="8"
-                        viewBox="0 0 12 8"
-                        fill="none"
-                        xmlns="http://www.w3.org/2000/svg"
-                      >
-                        <path
-                          d="M9.4136 3.55008L7.1818 1.31828L7.8182 0.681885L11.1364 4.00008L7.8182 7.31828L7.1818 6.68189L9.4136 4.45008H0.75V3.55008H9.4136Z"
-                          fill="rgba(0, 0, 0, 0.5)"
-                        />
-                    </svg>
-
+                <svg width="12" height="8" viewBox="0 0 12 8" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <path d="M9.4136 3.55008L7.1818 1.31828L7.8182 0.681885L11.1364 4.00008L7.8182 7.31828L7.1818 6.68189L9.4136 4.45008H0.75V3.55008H9.4136Z" fill="rgba(0, 0, 0, 0.5)"/>
+                </svg>
             </a>
-            <a href="<?php echo get_term_link( $first_vaccat->term_id, 'vaccat'); ?>" class="profession__crumb">
-                <?php echo $first_vaccat->name; ?>
-                    <!-- <svg
-                        width="12"
-                        height="8"
-                        viewBox="0 0 12 8"
-                        fill="none"
-                        xmlns="http://www.w3.org/2000/svg"
-                      >
-                        <path
-                          d="M9.4136 3.55008L7.1818 1.31828L7.8182 0.681885L11.1364 4.00008L7.8182 7.31828L7.1818 6.68189L9.4136 4.45008H0.75V3.55008H9.4136Z"
-                          fill="rgba(0, 0, 0, 0.5)"
-                        />
-                    </svg> -->
+            <a href="<?php echo get_term_link( $first_specialization->term_id, $_modificator.'specialization'); ?>" class="profession__crumb">
+                <?php echo $first_specialization->name; ?>
+                <!-- <svg
+                    width="12"
+                    height="8"
+                    viewBox="0 0 12 8"
+                    fill="none"
+                    xmlns="http://www.w3.org/2000/svg"
+                    >
+                    <path
+                        d="M9.4136 3.55008L7.1818 1.31828L7.8182 0.681885L11.1364 4.00008L7.8182 7.31828L7.1818 6.68189L9.4136 4.45008H0.75V3.55008H9.4136Z"
+                        fill="rgba(0, 0, 0, 0.5)"
+                    />
+                </svg> -->
             </a>
         </div>
     </div>
     <a href="#" class="profession__more-info">
-    Посмотреть вакансию
-    
-                <svg
-                  width="14"
-                  height="10"
-                  viewBox="0 0 14 10"
-                  fill="none"
-                  xmlns="http://www.w3.org/2000/svg"
-                >
-                  <path
-                    d="M11.5515 4.39995L8.57574 1.42421L9.42426 0.575684L13.8485 4.99995L9.42426 9.42421L8.57574 8.57568L11.5515 5.59995H0V4.39995H11.5515Z"
-                    fill="black"
-                  />
-                </svg>
+        Посмотреть вакансию
+        <svg width="14" height="10" viewBox="0 0 14 10" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path d="M11.5515 4.39995L8.57574 1.42421L9.42426 0.575684L13.8485 4.99995L9.42426 9.42421L8.57574 8.57568L11.5515 5.59995H0V4.39995H11.5515Z" fill="black"/>
+        </svg>
     </a>
     
     <div class="profession__job-price-container">
         <p class="profession__job-price">
-            от
-            <span class="profession__job-price-value"><?php echo number_format( get_field( 'money_from', $vacancy_item_id ), 0, ',', ' '); ?></span>
-            <span class="profession__currency">&#8381;</span>
-            <?php if( !get_field( 'not_gross', $vacancy_item_id) ): ?>
-                <span> - Гросс</span>
-            <?php endif; ?>
+            <?php if( get_field( 'money_from', $vacancy_item_id ) ){ ?>
+                от
+                <span class="profession__job-price-value"><?php echo number_format( get_field( 'money_from', $vacancy_item_id ), 0, ',', ' '); ?></span>
+                <span class="profession__currency">&#8381;</span>
+                <?php if( !get_field( 'not_gross', $vacancy_item_id) ): ?>
+                    <span> - Гросс</span>
+                <?php endif; ?>
+            <?php } ?>
         </p>
 
-
-        <?php //var_dump(get_field( 'can_without_experience', $vacancy_item_id ));
-        if( get_field( 'can_without_experience', $vacancy_item_id ) ){
-            ?>
+        <?php if( get_field( 'can_without_experience', $vacancy_item_id ) ){ ?>
             <p class="profession__job-expiriens"><?php echo get_field( 'can_without_experience', $vacancy_item_id )['label']; ?></p>
-            <?php
-        }
-        ?>
+        <?php } ?>
     </div>
 
     <div class="profession__location-container">
         <?php
-        $cur_terms = get_the_terms( get_the_ID(), 'town' );
-        if( is_array( $cur_terms ) ){
-            foreach( $cur_terms as $cur_term ){
+        $city_terms = get_the_terms( $vacancy_item_id, $_modificator.'city' );
+        if( is_array( $city_terms ) ){
+            foreach( $city_terms as $city_term ){
                 ?>
-                <p class="profession__location-city"><?php echo $cur_term->name; ?></p>
+                <p class="profession__location-city"><?php echo $city_term->name; ?></p>
                 <?php
             }
 
-            if( true == get_field( 'check_metro', $vacancy_item_id ) ){
-
-                $metro = get_field( 'metro_name', $vacancy_item_id );
-                echo $metro;
+            $metro_terms = get_the_terms( $vacancy_item_id, $_modificator.'metro' );
+            if( is_array( $metro_terms ) ){
+                foreach( $metro_terms as $metro_term ){
+                    echo $metro_term->name;
+                }
             }
         }
         ?>
@@ -111,7 +89,6 @@ if( is_array( $vaccat_terms ) ){
         ?>
         <!-- <a href="#" class="profession__location-map">
             На карте
-        
             <svg width="14" height="10" viewBox="0 0 14 10" fill="none" xmlns="http://www.w3.org/2000/svg">
                 <path d="M11.5515 4.39995L8.57574 1.42421L9.42426 0.575684L13.8485 4.99995L9.42426 9.42421L8.57574 8.57568L11.5515 5.59995H0V4.39995H11.5515Z" fill="black"/>
             </svg>
