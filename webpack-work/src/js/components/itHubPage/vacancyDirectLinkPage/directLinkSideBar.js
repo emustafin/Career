0817d851f51e51cd863e5d53da67ccf1d18be8ee
kosmich02144 <Pa9buqyz>
2022-02-02 14:13:26 +0300

    Исправляет ошибку копирования ссылки по кнопке part 2

--- webpack-work/src/js/components/itHubPage/vacancyDirectLinkPage/directLinkSideBar.js
+++ webpack-work/src/js/components/itHubPage/vacancyDirectLinkPage/directLinkSideBar.js
@@ -20,9 +20,6 @@ export class VacancyDirecLinkSideBar {
     );
     this.contentBarResponseButton = this.contentBar.contentResponseButton;
     this.contentBarFormBlock = this.contentBar.form;
-    this.copyLinkButton = this.el.querySelector(
-      '.direct-link__header-head-copy'
-    );
     this.copyLinkButtonMobile = this.contentBar.copyLinkButtonMobile;
     this.header = vacancyDirectLinkHeader || headerItPage;
 
@@ -31,24 +28,12 @@ export class VacancyDirecLinkSideBar {
       'click',
       this.scrollToForm.bind(this)
     );
-    this.copyLinkButton.addEventListener('click', this.copyLink.bind(this));
-    this.copyLinkButtonMobile.addEventListener(
-      'click',
-      this.copyLink.bind(this)
-    );
     window.addEventListener('scroll', this.manageSideBar.bind(this));
     window.addEventListener('resize', this.monitorScreenWidth.bind(this));
   }
   scrollToForm(event) {
     event.preventDefault();
     smothScrollingToBlock(this.form, this.header);
-  }
-
-  async copyLink(event) {
-    event.preventDefault();
-    const link = document.URL;
-
-    await navigator.clipboard.writeText(link);
   }
 
   manageSideBar() {
