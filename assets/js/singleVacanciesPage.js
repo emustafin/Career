b2cd27e0d15640c5c359939493b5e7fc97c3a1d6
kosmich02144 <Pa9buqyz>
2022-03-03 18:41:07 +0300

    Правки по инпуту в ваканисях

--- assets/js/singleVacanciesPage.js
+++ assets/js/singleVacanciesPage.js
@@ -1,8 +1,10 @@
 var townFormRetail = document.querySelector('input[name=text-town_r]');
+var curretnVacancyTowns = JSON.parse(vacancy_towns);
+console.log(curretnVacancyTowns);
 // TODO Сделать белый лист городов только относящиеся к розинце 
 if(townFormRetail != null){
     new Tagify(townFormRetail, {
-      whitelist: town_titles,
+      whitelist: curretnVacancyTowns,
       dropdown: {
       position: "text",
       maxItems: 5,
