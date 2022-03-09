3a43aa529f906052164f043f6ef73f4202b1c518
Mel-SON <omelyanchenko.semen@yandex.ru>
2022-03-09 16:57:06 +0300

    Решает баги с "показать еще" на архивных страницах + убирает лишнее

--- archive-vacancies.php
+++ archive-vacancies.php
@@ -527,7 +527,7 @@ else:
 endif; ?>
 <!-- Button Show-more -->
 <div class="products__show-more" style="<?php echo $button_show_more_display; ?>">
-    <div href="#" class="position__show-more-button">
+    <div class="position__show-more-button">
         показать ещё
         <svg class="position__show-more-button-link" width="10" height="10" viewBox="0 0 10 10" fill="none" xmlns="http://www.w3.org/2000/svg">
             <path d="M4.40039 5.60002V9.5H5.60039V5.60002H9.5V4.40002H5.60039V0.5H4.40039V4.40002H0.5L0.5 5.60002H4.40039Z" fill="black"/>
--- assets/js/ajax.js
+++ assets/js/ajax.js
@@ -228,19 +228,37 @@ document.addEventListener("DOMContentLoaded", function(event) {
 
         // document.querySelector('#actually_vacancies').innerHTML = '<div class="loader-bg"><div class="lds-ripple"><div></div><div></div></div></div>';
         
-        var ndi = JSON.stringify(newDefaultIcons);
-        var nmvi = JSON.stringify(mvideoIcons);
-        var neli = JSON.stringify(eldoradoIcons);
+        if( typeof newDefaultIcons != 'undefined' ){
+            var ndi = JSON.stringify(newDefaultIcons);
+        }
+        if( typeof mvideoIcons != 'undefined' ){
+            var nmvi = JSON.stringify(mvideoIcons);
+        }
+        if( typeof eldoradoIcons != 'undefined' ){
+            var neli = JSON.stringify(eldoradoIcons);
+        }
 
         var data = {
             action: 'archive_show_more_items',
-            newDefaultIcons : ndi,
-            nmvi : nmvi,
-            neli : neli,
-            shop_terms_id : shop_terms_id,
             query_vars : query_vars,
             paged : paged
         };
+
+        if( ndi ){
+            data['newDefaultIcons'] = ndi;
+        }
+
+        if( typeof shop_terms_id != 'undefined' ){
+            data['shop_terms_id'] = shop_terms_id;
+        }
+
+        if( nmvi ){
+            data['nmvi'] = nmvi;
+        }
+
+        if( neli ){
+            data['neli'] = neli;
+        }
 
         paged++;
 
