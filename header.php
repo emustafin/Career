1bf422bed8a0725ecebb79d09d3c5842040d6d37
kosmich02144 <Pa9buqyz>
2022-03-31 18:08:17 +0300

    Правит GTM

--- header.php
+++ header.php
@@ -15,6 +15,13 @@
     />
   <!-- //Подключение библиотеки Tagify -->
 
+  <!-- Google Tag Manager -->
+  <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+  new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+  j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+  'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+  })(window,document,'script','dataLayer','GTM-5KK9TMS');</script>
+  <!-- End Google Tag Manager -->
 
   <link href="<?php echo THEME_URL; ?>/assets/images/favicon/favicon.svg" rel="shortcut icon" type="image/x-icon"/>
   <title>М.ВидеоЭльдорадо</title>
@@ -61,7 +68,7 @@ $hformTowns = file_get_contents(get_template_directory().'/assets/towns/townsHfo
 <body class='<?php echo $body_class; ?>'>
 
 <!-- Google Tag Manager (noscript) -->
-<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-6ZQL"
+<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-5KK9TMS"
 height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
 <!-- End Google Tag Manager (noscript) -->
 
